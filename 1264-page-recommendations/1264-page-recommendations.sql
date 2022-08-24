1a51460106a18e8ffcaaa22ac0c5ca06cbe719ff
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-08-24 12:01:21 -0300

    Time: 584 ms (60.77%), Space: 0B (100.00%) - LeetHub

---
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT DISTINCT page_id as recommended_page
+FROM Likes
+WHERE page_id NOT IN(SELECT page_id FROM Likes WHERE user_id=1) AND user_id IN ((SELECT user2_id FROM Friendship WHERE user1_id=1) UNION (SELECT user1_id FROM Friendship WHERE user2_id=1))
+
