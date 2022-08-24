e574bec94c2d2f6b447354f682c546c8a4634527
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-08-24 11:50:38 -0300

    Time: 1150 ms (48.50%), Space: 0B (100.00%) - LeetHub

---
+++ 1767-find-the-subtasks-that-did-not-execute/1767-find-the-subtasks-that-did-not-execute.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+
+WITH RECURSIVE CTE AS(
+     SELECT task_id, subtasks_count
+     FROM Tasks
+     UNION ALL
+     SELECT task_id, subtasks_count-1
+     FROM CTE
+     WHERE subtasks_count>1
+)
+
+SELECT task_id, subtasks_count as subtask_id
+FROM CTE
+WHERE (task_id, subtasks_count) NOT IN (SELECT* FROM Executed)
