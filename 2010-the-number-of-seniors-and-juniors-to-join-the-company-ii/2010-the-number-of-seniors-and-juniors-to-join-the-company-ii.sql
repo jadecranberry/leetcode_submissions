2994e892b7f37c0b0b92c851199fe20f826cf3b0
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-08-24 11:50:12 -0300

    Time: 1362 ms (5.78%), Space: 0B (100.00%) - LeetHub

---
+++ 2010-the-number-of-seniors-and-juniors-to-join-the-company-ii/2010-the-number-of-seniors-and-juniors-to-join-the-company-ii.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+
+
+WITH CTE AS(
+    SELECT employee_id, experience, SUM(salary) OVER(PARTITION BY experience ORDER BY salary ASC) AS RN 
+    FROM Candidates
+)
+
+SELECT employee_id FROM CTE WHERE experience='senior' AND RN<70000
+UNION
+SELECT employee_id FROM CTE WHERE experience='junior' AND RN<(SELECT 70000-IFNULL(MAX(RN),0) FROM CTE WHERE experience='senior' AND RN<70000)
