cd485c983368a5eb5ff5bc64a8dfe0acf48d358c
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-08-29 07:35:58 -0300

    Time: 1976 ms (10.98%), Space: 20.1 MB (53.16%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,20 @@
+#from collections input defaultdict
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
+        max_fruit = 0
+        hash = {}
+        for right, value in enumerate(fruits):
+            if value not in hash:
+                hash[value]=0
+            hash[value]+=1
+ 
+            while len(hash)>2:
+                hash[fruits[left]]-=1
+                if hash[fruits[left]]==0:
+                    del hash[fruits[left]]
+                left+=1
+                
+            max_fruit = max(max_fruit, right-left+1)    
+        return max_fruit         
+        
