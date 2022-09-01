2a2e92fab30778c6c4fa0da360b84c10ed04dc03
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-09-01 11:52:31 -0300

    Time: 114 ms (45.11%), Space: 14 MB (93.23%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,20 +1,16 @@
 class Solution:
     #sliding window optimized solution
     def lengthOfLongestSubstring(self, s: str) -> int:
-        chars = [None]*128
-        left = right = 0
+        left = 0
+        maxlength = 0            
+        hash = {}
         
-        res = 0
-        while right<len(s):
-            r = s[right]
-            #use hashmap to store the index of a letter
-            index = chars[ord(r)]
-            #check if the index of this letter already in chars, if yes, start new search begining on index+1
-            if index != None and index >= left and index<right:
-                left = index+1
-                
-            res = max(res, right-left+1)
+        for right, value in enumerate(s):
+            if value in hash:
+                left = max(left, hash[value] + 1)
+            hash[value]=right
+            maxlength=max(maxlength, right-left+1)
+           
+        return maxlength  
+
             
-            chars[ord(r)]=right
-            right += 1
-        return res    
