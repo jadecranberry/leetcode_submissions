887fc0d0e471a1788091073ddce6ffb505bbd1f3
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-09-22 11:41:16 -0300

    Time: 1723 ms (27.59%), Space: 13.9 MB (89.59%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.py
+++ 259-3sum-smaller/259-3sum-smaller.py
@@ -1,3 +1,5 @@
+#two pointers. similar to leetcode: 16. 3Sum Closest
+
 class Solution:
     def threeSumSmaller(self, nums: List[int], target: int) -> int:
         nums.sort()
@@ -10,6 +12,8 @@ class Solution:
                 if sums>=target:
                     r-=1
                 if sums<target:
+                   # since nums[right] >= nums[left], therefore, we can replace nums[right] by any number between
+                   # left and right to get a sum less than the target sum
                     count+=r-l
                     l+=1
         return count            
