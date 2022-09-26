78219e1e250479d6d92070959023e304410f9a10
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-09-26 11:50:44 -0300

    Time: 61 ms (89.07%), Space: 17.5 MB (94.23%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -1,11 +1,11 @@
-
+#If there is cycle in the list, we can prove that the fast and slow pointers will eventually meet for sure.
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
         while fast is not None and fast.next is not None:
-            fast=fast.next.next
-            slow=slow.next
+            slow = slow.next
+            fast = fast.next.next
             if slow == fast:
                 return True
         return False    
