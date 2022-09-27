2334b37225936d9714fd56c0d78034f2d2a336a6
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-09-27 16:03:33 -0300

    Time: 190 ms (13.86%), Space: 17.6 MB (50.12%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,5 +1,4 @@
 
-
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if not intervals:
@@ -10,6 +9,7 @@ class Solution:
         heapq.heappush(min_rooms, intervals[0][1])
         
         for interval in intervals[1:]:
+            #if the start time of next interval is bigger than the earliest finish time of a previous meeting, free this meeting room
             if interval[0]>=min_rooms[0]:
                 heapq.heappop(min_rooms)
             heapq.heappush(min_rooms, interval[1])
