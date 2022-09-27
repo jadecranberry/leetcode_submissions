9b51538dd5848811ca162c9745447d9219d5e911
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-09-26 22:42:43 -0300

    Time: 1641 ms (21.21%), Space: 46.5 MB (55.73%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+#copy all values in linked list to arry vals and then compare the values with the reversed values
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         vals=[]
@@ -10,4 +12,5 @@ class Solution:
         while current_node:
             vals.append(current_node.val)
             current_node=current_node.next
+            #vals[::-1] reverses vals
         return vals==vals[::-1]
