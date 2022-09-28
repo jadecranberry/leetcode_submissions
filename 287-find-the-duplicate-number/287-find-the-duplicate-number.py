76f6a7d7864054f750f38938df716aee3b7115fb
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-09-28 10:50:28 -0300

    Time: 1621 ms (14.50%), Space: 28 MB (34.67%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,13 +1,17 @@
 #fast and slow pointers
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        #the following finds the itersection points for slow and fast pointers
+        #it should be noted the intersection point is not the cycle entrance point
         slow=fast=nums[0]
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow==fast:
                 break
-                
+        
+        #the following finds the cycle entrance point by starting the slow point at the begining 
+        #and slow down the fast point, when they meet again it is the cycle entrance
         slow = nums[0]
         while slow!=fast:
             slow = nums[slow]
