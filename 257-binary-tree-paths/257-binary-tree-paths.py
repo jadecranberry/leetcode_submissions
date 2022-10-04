02e011c7e33a79ac47b66797c180bbc5bb975009
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-10-04 10:14:19 -0300

    Time: 67 ms (20.90%), Space: 13.8 MB (75.12%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        allPaths=[]
+        if root is None:
+            return
+        self.binaryTreePath_recursive(root, '', allPaths)
+        return allPaths
+        
+    def binaryTreePath_recursive(self, currentNode, currentPath, allPaths):
+        if currentNode is None:
+            return
+        currentPath+=str(currentNode.val)
+        
+        if currentNode.left is None and currentNode.right is None:
+            allPaths.append(currentPath)
+        else:
+            currentPath+='->'
+            self.binaryTreePath_recursive(currentNode.left, currentPath, allPaths)
+            self.binaryTreePath_recursive(currentNode.right, currentPath, allPaths)
+          
+            
