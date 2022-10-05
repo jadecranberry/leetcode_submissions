02cb193bee8b4e608934ffc862d47507d8c22b63
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-10-05 15:54:51 -0300

    Time: 1366 ms (77.40%), Space: 38.5 MB (77.03%) - LeetHub

--- 502-ipo/502-ipo.py
+++ 502-ipo/502-ipo.py
@@ -1,18 +1,23 @@
+#use two heaps
 from heapq import *
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        #use min heap to store the capitals, use max heap to store the profits
         minCapitalHeap = []
         maxProfitHeap = []
         
+        #populate the min heap
         for i in range(len(capital)):
             heappush(minCapitalHeap, (capital[i], i))
             
         availableCapital = w
+        #since we can only choose k projects.
         for _ in range(k):
+            #populate the max heap with all the possible projects we can choose with the availabelCapital
             while minCapitalHeap and minCapitalHeap[0][0]<=availableCapital:
                 captial, i = heappop(minCapitalHeap)
                 heappush(maxProfitHeap, (-profits[i], i))
-                
+            #break if we can't find any project with the avaialble capital    
             if not maxProfitHeap:
                 break
                 
