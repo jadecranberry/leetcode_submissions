b7a5b0c51ab455d462cd9463304fccfc4d08a12c
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-10-06 06:51:52 -0300

    Time: 72 ms (19.03%), Space: 14.2 MB (35.69%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,17 +1,3 @@
-def find_subsets(nums):
-  subsets = []
-  # start by adding the empty subset
-  subsets.append([])
-  for currentNumber in nums:
-    # we will take all existing subsets and insert the current number in them to create new subsets
-    n = len(subsets)
-    for i in range(n):
-      # create a new subset from the existing subset and insert the current element to it
-      set1 = list(subsets[i])
-      set1.append(currentNumber)
-      subsets.append(set1)
-
-  return subsets
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result=[]
