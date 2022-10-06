a6be9089a094e44904f8ca3c5ba928a49157bb76
jadecranberry <32283596+jadecranberry@users.noreply.github.com>
2022-10-06 12:45:04 -0300

    Time: 23 ms (99.90%), Space: 14.4 MB (5.73%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result=[]
+        queue = deque()
+        queue.append(ParenthesesString("", 0, 0))
+        while queue:
+            ps=queue.popleft()
+            if ps.openCount==n and ps.closeCount==n:
+                result.append(ps.str)
+            else:
+                if ps.openCount<n:
+                    queue.append(ParenthesesString(ps.str+"(", ps.openCount+1, ps.closeCount))
+                if ps.openCount>ps.closeCount:
+                    queue.append(ParenthesesString(ps.str+")", ps.openCount, ps.closeCount+1))
+        return result            
+        
+class ParenthesesString:    
+    def __init__(self, str, openCount, closeCount):
+        self.str = str
+        self.openCount = openCount
+        self.closeCount = closeCount
